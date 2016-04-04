2cbf0eeadc2cb5e84e2b37070857e420d4f36887
bh_lay <bh_lay@126.com>
2016-04-04 19:48:37 +0800

    调整音乐播放部分

--- js/music.js
+++ js/music.js
@@ -1,21 +1,30 @@
-(function(){
-	// var music = new Audio();
-	// music.autoplay = 'autoplay="autoplay"';
-	// music.loopPlay = 'loop="loop"';
-	// music.src = '/mp3/LoveParadise.mp3';
-	// function play(){
-	// 	music.play();
-	// 	document.removeEventListener('touchstart',play , false);
-	// }
-	// music.play();
-	// document.addEventListener('touchstart', play, false);
-	// document.querySelector('.music').addEventListener('click',function(){
-	// 	if (music.paused) {
-	// 		music.play();
-	// 		this.classList.add('play');
-	// 	} else {
-	// 		music.pause();
-	// 		this.classList.remove('play');
-	// 	}
-	// });
-})();
+function backgroundMusic(){
+	var music = new Audio(),
+		music_btn = document.querySelector('.music');
+
+	music.addEventListener('play',function(){
+		music_btn.classList.add('play');
+	});
+	music.addEventListener('pause',function(){
+		music_btn.classList.remove('play');
+	});
+	//设置音乐属性
+	music.autoplay = 'autoplay="autoplay"';
+	music.loopPlay = 'loop="loop"';
+	music.src = '/mp3/LoveParadise_C48kbps.mp3';
+	
+	music.play();
+
+	function mustPlayFirst(){
+		music.play();
+		document.removeEventListener('touchstart',mustPlayFirst , false);
+	}
+	document.addEventListener('touchstart', mustPlayFirst, false);
+	music_btn.addEventListener('click',function(){
+		if (music.paused) {
+			music.play();
+		} else {
+			music.pause();
+		}
+	});
+}
